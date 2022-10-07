7b9474d51f19346ddc0298dd2eee7c6af09087d4
Marcos Mariatti <m.marky85@gmail.com>
2022-10-07 18:18:38 -0300

    agrego server.js

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 'use strict';
-
+ 
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
+ /* Importamos la ruta a oauth y lo necesario para autenticar con passport */
+const passportSetup = require("./config/passport-setup");
+const passport = require("passport");
+const authRoutes = require("./routers/auth");
+
+// Inicializamos la app
+const app = express();
+
+// Inicializamos el passport
+app.use(passport.initialize());
+app.use(passport.session());
+app.use("/auth", authRoutes);
+
+/* Conexion a la base de datos */
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log("Database Connected"))
 .catch((err) => console.log(err));
 
@@ -14,9 +28,23 @@ const PORT = 8080;
 const HOST = '0.0.0.0';
 
 // App
-const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+//route not found
+app.use((req, res, next) => {
+  const error = new Error("Route not found");
+  error.status = 404;
+  next(error);
+});
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
 });
 
 app.listen(PORT, HOST, () => {
