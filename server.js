edcb5e91d6054303eca0d72ac93a9e7a6518766d
Marcos Mariatti <m.marky85@gmail.com>
2022-10-07 16:51:58 -0300

    Agrego conexion a la BD

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 'use strict';
 
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log("Database Connected"))
+.catch((err) => console.log(err));
+
 
 // Constants
 const PORT = 8080;
