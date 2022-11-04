b56a05741c1c813e7755e5080533e67b0b4583bc
Marcos Mariatti <m.marky85@gmail.com>
2022-11-04 19:49:14 -0300

    Correccion de paquetes

--- src/server.js
+++ src/server.js
@@ -1,17 +1,27 @@
-'use strict';
+//'use strict';
  
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 require('dotenv').config({path: '../.env'});
+const session = require('express-session');
+// Inicializamos la app
+const app = express();
+
+//configuramos la session
+app.use(session({
+  secret: 'demo',
+  resave: false,
+  saveUninitialized: true,
+}));
 
  /* Importamos la ruta a oauth y lo necesario para autenticar con passport */
 const passportSetup = require("./config/passport-setup");
 const passport = require("passport");
 const authRoutes = require("./routers/auth");
 
-// Inicializamos la app
-const app = express();
+
+
 
 // Inicializamos el passport
 app.use(passport.initialize());
@@ -19,8 +29,13 @@ app.use(passport.session());
 app.use("/auth", authRoutes);
 
 /* Conexion a la base de datos */
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log("Database Connected"))
-.catch((err) => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log(err));
 
 mongoose.Promise = global.Promise;
 
@@ -34,7 +49,8 @@ const HOST = process.env.HOST
 
 // App
 app.get('/', (req, res) => {
-  res.send('Hello World');
+
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 //route not found
