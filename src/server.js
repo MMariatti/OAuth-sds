b086fc91029de81f62adebce5b1257846e4803b4
Nehuen Olivera <nehuenolivera@gmail.com>
2022-11-06 11:46:45 -0300

    Cambio de variable secret en server.js y renovacion de index

--- src/server.js
+++ src/server.js
@@ -1,27 +1,26 @@
 //'use strict';
- 
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-require('dotenv').config({path: '../.env'});
-const session = require('express-session');
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config({ path: "../.env" });
+const session = require("express-session");
 // Inicializamos la app
 const app = express();
 
 //configuramos la session
-app.use(session({
-  secret: process.env.SECRET,
-  resave: false,
-  saveUninitialized: true,
-}));
+app.use(
+  session({
+    secret: "demo",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
- /* Importamos la ruta a oauth y lo necesario para autenticar con passport */
+/* Importamos la ruta a oauth y lo necesario para autenticar con passport */
 const passportSetup = require("./config/passport-setup");
 const passport = require("passport");
 const authRoutes = require("./routers/auth");
-
-
-
 
 // Inicializamos el passport
 app.use(passport.initialize());
@@ -42,15 +41,13 @@ mongoose.Promise = global.Promise;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
 // Constants
-const PORT = process.env.PORT
-const HOST = process.env.HOST
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
 
 // App
-app.get('/', (req, res) => {
-
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 //route not found
